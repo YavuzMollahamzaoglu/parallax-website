bd8f39cfb7f9b3f2e03ec46b599baebc7138cdb2
Yavuz Mollahamzaoğlu <yavuz.mersin@hotmail.com>
2022-09-12 20:45:36 +0300

    add parallax website css

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <div className="main">
+        <div className="main-img"></div>
+        <div className="main-head">
+          <h2 className="border">PARALLAX WEBSITE</h2>
+        </div>
+      </div>
+      <div className="section-one">
+        <h2>Section One</h2>
+        <div className="h2-info">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt,
+          laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora
+          facere dolores nisi facilis consequatur, odio hic minima nostrum.
+          Perferendis eos earum praesentium, blanditiis sapiente labore aliquam
+          ipsa architecto vitae. Minima soluta temporibus voluptates inventore
+          commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate
+          nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat
+          quisquam blanditiis voluptatem doloremque fugiat dolor repellendus
+          ratione in. Distinctio provident dolorem modi cumque illo enim quidem
+          tempora deserunt nostrum voluptate labore repellat quisquam quasi cum
+          suscipit dolore ab consequuntur, ad porro earum temporibus. Laborum ad
+          temporibus ex, omnis!
+        </div>
+        <div className="section-one-p">
+          <div className="one-p"></div>
+          <div className="main-head">
+            <h2>IMAGE TWO TEXT</h2>
+          </div>
+        </div>
+      </div>
+      <div className="section-two">
+        <div className="section-two-p">
+          <div className="two-p"></div>
+          <div className="main-head">
+            <h2>IMAGE THREE TEXT</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
